2ba71bdacc63eec3f06b52dcc5a5706764dd01b4
Florent <florentfraysse4@gmail.com>
2023-05-23 18:06:20 +0200

    login

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('login-form')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const error = document.getElementById('error')
+
+loginForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    console.log(email.value, password.value)
+
+    fetch('http://localhost:5678/api/users/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'email': email.value, 'password': password.value })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            if (data.token) {
+                // TODO save token localstorage see: https://developer.mozilla.org/fr/docs/Web/API/Window/localStorage
+
+                // Redirect to home page
+                window.location.href = `${window.location.origin}/index.html`
+            } else {
+                error.textContent = "Erreur dans l’identifiant ou le mot de passe"
+            }
+
+        })
+})
